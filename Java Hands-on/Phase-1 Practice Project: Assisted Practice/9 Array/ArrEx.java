7d59f1b7bb98b0c4348dcb48dd6714bc9fac044f
prathijnaudupa <109268619+prathijnaudupa@users.noreply.github.com>
2022-07-20 12:28:26 +0530

    Add files via upload

---
+++ Java Hands-on/Phase-1 Practice Project: Assisted Practice/9 Array/ArrEx.java
@@ -0,0 +1,14 @@
+class ArrEx{  
+public static void main(String args[]){  
+int a[]=new int[5];  
+a[0]=3; 
+a[1]=6;  
+a[2]=4;  
+a[3]=17;  
+a[4]=1;  
+System.out.println("Array elements are :");
+for(int i=0;i<a.length;i++)  
+System.out.println(a[i]);  
+System.out.println("value at index 3 is " + a[3]);
+System.out.println("length of the array is :"+ a.length);
+}}  
